eea99d3975b3e92d66fcbbcb32ffbb77f1887b85
Нигматжонов Азизжон Нодиржон <aziz.nigmatjonov7@gmail.com>
2025-10-08 11:39:52 +0500

    fixed

--- app/home/instruction.tsx
+++ app/home/instruction.tsx
@@ -1,15 +1,16 @@
 import { ThemedView } from "@/components/ThemedView";
 import HeaderUI from "@/components/UI/Header";
-import { Dimensions } from "react-native";
+import { Dimensions, View } from "react-native";
+import { globalStyles } from "@/components/UI/GlobalStyles";
 
 export default function TabTirdScreen() {
   const { height: SCREEN_HEIGHT } = Dimensions.get("window");
   return (
-    <ThemedView>
+    <ThemedView style={{ height: SCREEN_HEIGHT }}>
       <HeaderUI place="Инструкция" />
-      <ThemedView style={{ height: SCREEN_HEIGHT, padding: 12 }}>
-        Пока нет инструкции
-      </ThemedView>
+      <View style={globalStyles.container}>
+        <ThemedView style={{ padding: 12 }}>Пока нет инструкции</ThemedView>
+      </View>
     </ThemedView>
   );
 }
